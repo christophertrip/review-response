5934e1b5b9dfd5b03ad5f0c2ea554f1182e8a26a
Christopher Trip <110012450+christophertrip@users.noreply.github.com>
2023-07-17 19:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ guest_review = st.text_area('Guest Review', placeholder="Paste your Guest's revi
 if type_of_host == "Home Stay 🏠":
     sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'You always have a home here with us in Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
 else:
-    sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Pura Vida 🇨🇷!', placeholder="Custom text", label_visibility="visible", help="Example: _Thanks for coming on the adventure!_")
+    sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Pura Vida! 🇨🇷', placeholder="Custom text", label_visibility="visible", help="Example: _Thanks for coming on the adventure!_")
 
 response_language = st.selectbox('Choose response language:', ('English 🇺🇸', 'Spanish 🇪🇸', 'French 🇫🇷'), index=0)
 
