399bb101414269007c8f210e6094fc6e892e3927
Christopher Trip <110012450+christophertrip@users.noreply.github.com>
2023-11-02 18:27:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,6 @@ if st.button('Start the Magic  🪄'):
         st.divider()
         st.subheader('Review Response')
         st.code(review_response, language=None)
-	
-st.code(guest_review, language=None)
 
 ################################### Define your JavaScript ########################################
 
