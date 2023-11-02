f4f31b76cf16cde9995b878fecc5522caff73399
Christopher Trip <110012450+christophertrip@users.noreply.github.com>
2023-11-02 15:24:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,33 +7,34 @@ st.set_page_config(page_title="Review Response App", page_icon="✍️")
 openai.api_key = st.secrets["OPENAI_API"]
 
 st.title('✍️ Review Response')
-type_of_host = st.selectbox('Choose type of Host:', ('Home Stay 🏠', 'Airbnb Experience 🏄‍♂️'), index=0, help="Choose if this is for a Home Stay or for an Airbnb Experience")
+#type_of_host = st.selectbox('Choose type of Host:', ('Home Stay 🏠', 'Airbnb Experience 🏄‍♂️'), index=0, help="Choose if this is for a Home Stay or for an Airbnb Experience")
 # hosts = st.checkbox('Chris & Andrea')
-if type_of_host == "Home Stay 🏠":
-	host_name = st.text_input('Host Name', 'Chris, Andrea & Mitchell', placeholder="Host name", label_visibility="collapsed")
-else:
-	host_name = st.text_input('Host Name', placeholder="Host name", label_visibility="collapsed")
+#if type_of_host == "Home Stay 🏠":
+#host_name = st.text_input('Host Name', 'Chris, Andrea & Mitchell', placeholder="Host name", label_visibility="collapsed")
+host_name = "Chris, Andrea & Mitchell"
+#else:
+	#host_name = st.text_input('Host Name', placeholder="Host name", label_visibility="collapsed")
 guest_name = st.text_input('Guest Name', placeholder="Guest name", label_visibility="collapsed")
 guest_review = st.text_area('Guest Review', placeholder="Paste your Guest's review here.", label_visibility="collapsed", height=150)
 
-response_language = st.selectbox('Choose response language:', ('English 🇺🇸', 'Spanish 🇲🇽', 'French 🇫🇷'), index=0)
+# response_language = st.selectbox('Choose response language:', ('English 🇺🇸', 'Spanish 🇲🇽', 'French 🇫🇷'), index=0)
 
 # Different for Homes and Experiences
-if type_of_host == "Home Stay 🏠":
-	if response_language == "English 🇺🇸":
-    		sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'You always have a home here with us in Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
-	elif response_language == "Spanish 🇲🇽":
-    		sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', '¡Siempre tienes un hogar aquí con nosotros en Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
-	else:
-    		sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Vous avez toujours une maison ici avec nous à Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
-else:
-    sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Pura Vida! 🇨🇷', placeholder="Custom text", label_visibility="visible", help="Example: _Thanks for coming on the adventure!_")
+#if type_of_host == "Home Stay 🏠":
+	#if response_language == "English 🇺🇸":
+    		#sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'You always have a home here with us in Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
+	#elif response_language == "Spanish 🇲🇽":
+    		#sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', '¡Siempre tienes un hogar aquí con nosotros en Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
+	#else:
+    		#sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Vous avez toujours une maison ici avec nous à Playa!', placeholder="Custom text", label_visibility="visible", help="Example: _You always have a home here with us!_")
+#else:
+    #sign_off_text = st.text_input('Custom text at the end of the response: _(Optional)_', 'Pura Vida! 🇨🇷', placeholder="Custom text", label_visibility="visible", help="Example: _Thanks for coming on the adventure!_")
 
 # Different for Homes and Experiences
-if type_of_host == "Home Stay 🏠":
-    messages = [{"role": "system", "content": f'We are Airbnb hosts named {host_name}. We just had one of our guests named {guest_name} leave a very polite review. Please create a very polite reply based on their review in {response_language}, in 50 words or less.\n\n"{guest_review}"'}]
-else:
-    messages = [{"role": "system", "content": f'I am an Airbnb Experience host named {host_name}. A guest named {guest_name} left the following review. Please create a very polite reply based on their review in {response_language}, in 50 words or less.\n\n"{guest_review}"'}]
+#if type_of_host == "Home Stay 🏠":
+messages = [{"role": "system", "content": f'We are Airbnb hosts named {host_name}. We just had one of our guests named {guest_name} leave a very polite review. Please detect the language of the review and create a very polite reply in the same language as their review, in 50 words or less. At the end of the review please append the following text — You always have a home here with us in Playa! Please remember the whole reply needs to be in the same language as the guests review.\n\n"{guest_review}"'}]
+#else:
+   # messages = [{"role": "system", "content": f'I am an Airbnb Experience host named {host_name}. A guest named {guest_name} left the following review. Please create a very polite reply based on their review in {response_language}, in 50 words or less.\n\n"{guest_review}"'}]
 
 # if button clicked then do the with st.spinner
 if st.button('Start the Magic  🪄'):
