2db576d6e66147d61b026a002d0c7983bfe2d9bd
Christopher Trip <110012450+christophertrip@users.noreply.github.com>
2024-05-30 20:08:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,7 @@ messages = [{"role": "system", "content": f'Hello wonderful assistant! We are Ai
 if st.button('Start the Magic  🪄'):
 
     with st.spinner(f"Creating your Reponse..."): 
-        #response = openai.ChatCompletion.create(model="gpt-4o", messages=messages)
 	response = client.chat.completions.create(model="gpt-4o", messages=messages)
-        #review_response = f'{response["choices"][0]["message"]["content"]}'
 	review_response = f'{response.choices[0].message.content}'
         st.divider()
         st.subheader('Review Response')
